a61d48942c78dddd693f574aba61f8afee76b628
chentao <543922636@qq.com>
2017-08-04 13:55:47 +0800

    change test.py

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
-class student(object):
-	def __init__(self,name,score):
-		self.name=name
-		self.score=score
+import time, threading
 
-	def print_score(self):
-		print('%s:%s' % (self.name,self.score))
+# 假定这是你的银行存款:
+balance = 0
 
-bart=student('bart',59)
-lisa=student('lisa',87)
-bart.print_score()
-lisa.print_score()
-bart.sss=30
-print(bart.sss)
+def change_it(n):
+    # 先存后取，结果应该为0:
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+def run_thread(n):
+    for i in range(100000):
+        change_it(n)
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
